28ca519e0d4ca24e27302bdace256e1c653681ef
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-12-06 12:44:54 +0100

    Added five more tests in person_spec.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 RSpec.describe 'person.rb' do
   before :each do
     @person = Person.new :firstname, :lastname, :id
+    @tomasz = Person.new('Tomasz', 'Adamczyk', '94010112345')
   end
 
   describe '#new' do
     it 'method #new work?' do
       expect { @person }.not_to raise_error
+    end
+
+    it 'returns a person type from @tomasz object' do
+      expect(@tomasz).to be_an_instance_of(Person)
     end
   end
 
@@ -15,11 +20,19 @@ RSpec.describe 'person.rb' do
     it 'returns a correct firstname' do
       expect(@person.firstname).to eq(:firstname)
     end
+
+    it 'returns a correct firstname from @tomasz object' do
+      expect(@tomasz.firstname).to eq('Tomasz')
+    end
   end
 
   describe '#lastname' do
     it 'returns a correct lastname' do
       expect(@person.lastname).to eq(:lastname)
+    end
+
+    it 'returns a correct lastname from @tomasz object' do
+      expect(@tomasz.lastname).to eq('Adamczyk')
     end
   end
 
@@ -27,11 +40,19 @@ RSpec.describe 'person.rb' do
     it 'returns a correct id' do
       expect(@person.id).to eq(:id)
     end
+
+    it 'returns a correct id from @tomasz object' do
+      expect(@tomasz.id).to eq('94010112345')
+    end
   end
 
   describe '#to_s' do
     it 'method #to_s work?' do
       expect { @person.to_s }.not_to raise_error
+    end
+
+    it 'returns correctly string output from @tomasz object' do
+      expect(@tomasz.to_s).to eq('Tomasz Adamczyk')
     end
   end
 end
